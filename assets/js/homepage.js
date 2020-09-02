927acb325bdd97b6323660d72fa4799dfebb3497
Devin Gibson <dgib21@hotmail.com>
2020-09-02 12:24:51 -0500

    added catch block

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -42,16 +42,34 @@ var formSubmitHandler = function (event) {
 };
 
 var getUserRepos = function (user) {
-    // format the github api url
+    //format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    // make a request to the url
-    fetch(apiUrl).then(function (response) {
-        response.json().then(function (data) {
-            //console.log(data);
-            displayRepos(data, user);
+    //  //make a request to the url
+    //  fetch(apiUrl).then(function (response) {
+    //      response.json().then(function (data) {
+    //          //console.log(data);
+    //          displayRepos(data, user);
+    //      });
+
+    fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data, user);
         });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      alert("Unable to connect to GitHub");
     });
+
+  
+
 };
 
 
@@ -61,43 +79,49 @@ userFormEl.addEventListener("submit", formSubmitHandler);
 var displayRepos = function (repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
+// check if api returned any repos
+if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
+
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
 
     // loop over repos
-for (var i = 0; i < repos.length; i++) {
-    // format repo name
-    var repoName = repos[i].owner.login + "/" + repos[i].name;
-  
-    // create a container for each repo
-    var repoEl = document.createElement("div");
-    repoEl.classList = "list-item flex-row justify-space-between align-center";
-  
-    // create a span element to hold repository name
-    var titleEl = document.createElement("span");
-    titleEl.textContent = repoName;
-  
-    // append to container
-    repoEl.appendChild(titleEl);
-  
-// create a status element
-var statusEl = document.createElement("span");
-statusEl.classList = "flex-row align-center";
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-// check if current repo has issues or not
-if (repos[i].open_issues_count > 0) {
-  statusEl.innerHTML =
-    "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-} else {
-  statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-}
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
 
-// append to container
-repoEl.appendChild(statusEl);
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
 
-    // append container to the dom
-    repoContainerEl.appendChild(repoEl);
-  }
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
 
 };
